178c4f9b653911c58ad0545aba6d857c463df35b
Silviya Velani <silviya@fourplaysocial.com>
2021-03-03 18:51:10 -0500

    Add edit list functionality implemented

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
-/**
- * Add your code here to fix URI connection
- * 
- */
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://silviya:iR2AmeSsWDUbITuh@midtermmongodbserver.l2kpn.mongodb.net/book_store?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
